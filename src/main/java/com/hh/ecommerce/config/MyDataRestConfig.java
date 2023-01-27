0348f27f472beb7c2f5a2954c5c49f97e791b0ce
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-27 10:26:26 -0600

    2: Se agregan metodos para poder paginar, se agregan entidades para manejar Paises y Estados con sus respectivos repositorios para poder consultarlos desde la aplicacion frontend, se deshabilitan metodos POST, PUT y DELETE para las nuevas entidades

--- src/main/java/com/hh/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/hh/ecommerce/config/MyDataRestConfig.java
@@ -1,30 +1,74 @@
 package com.hh.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import com.hh.ecommerce.entity.Country;
 import com.hh.ecommerce.entity.Product;
 import com.hh.ecommerce.entity.ProductCategory;
+import com.hh.ecommerce.entity.State;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	private EntityManager entityManager;
+
+	@Autowired
+	public MyDataRestConfig(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		HttpMethod[] unsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
 
-//		disable HTTP methods for PRODUCTS: PUT, POST, DELETE
-		config.getExposureConfiguration().forDomainType(Product.class)
-				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
-				.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+//		disable HTTP methods for PRODUCTS, ProductCategory, Country and State: 
+//		PUT, POST, DELETE
+		disableHttpMethods(Product.class, config, unsupportedActions);
+		disableHttpMethods(ProductCategory.class, config, unsupportedActions);
+		disableHttpMethods(Country.class, config, unsupportedActions);
+		disableHttpMethods(State.class, config, unsupportedActions);
 
-//		disable HTTP methods for PRODUCT CATEGORY: PUT, POST, DELETE
-		config.getExposureConfiguration().forDomainType(ProductCategory.class)
+//		call an internal helper method
+		exposeIds(config);
+	}
+
+	private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config,
+			HttpMethod[] unsupportedActions) {
+
+		config.getExposureConfiguration().forDomainType(theClass)
 				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
 				.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+
+//		expose entity ids
+//		- get a list of all entity classes from the entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+//		create an array of the entity types
+		List<Class> entityClassess = new ArrayList<>();
+
+//		get the tntity types for the entities
+		for (EntityType entityType : entities) {
+			entityClassess.add(entityType.getJavaType());
+		}
+
+//		expose the entity ids for the array of entity/domain types
+		Class[] domainTypes = entityClassess.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 
 }
--- src/main/java/com/hh/ecommerce/dao/CountryRepository.java
+++ src/main/java/com/hh/ecommerce/dao/CountryRepository.java
@@ -4,10 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import com.hh.ecommerce.entity.ProductCategory;
+import com.hh.ecommerce.entity.Country;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 @CrossOrigin("http://localhost:4200")
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface CountryRepository extends JpaRepository<Country, Integer> {
 
 }
